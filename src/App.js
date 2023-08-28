d8d5300126228b04b4b248d8f92887fd1f4b1702
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-08-28 07:24:40 -0400

    useState with objects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
+import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <ClassCounter />
       <HookCounter />
       <HookCounterTwo />
+      <HookCounterThree />
     </div>
   );
 }
