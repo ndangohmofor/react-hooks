bfd7dfdb4df3cb831dfb940f8c2bfdfada5a8c9d
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-09-02 00:25:46 -0400

    Fetching data with useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-
-import './App.css';
-import ClassCounter from './components/ClassCounter';
-import ClassCounterOne from './components/ClassCounterOne';
-import ClassMouse from './components/ClassMouse';
-import HookCounter from './components/HookCounter';
-import HookCounterFour from './components/HookCounterFour';
-import HookCounterOne from './components/HookCounterOne';
-import HookCounterThree from './components/HookCounterThree';
-import HookCounterTwo from './components/HookCounterTwo';
-import HookMouse from './components/HookMouse';
-import IntervalClassCounter from './components/IntervalClassCounter';
-import IntervalHookCounter from './components/IntervalHookCounter';
-import MouseContainer from './components/MouseContainer';
+import "./App.css";
+import ClassCounter from "./components/ClassCounter";
+import ClassCounterOne from "./components/ClassCounterOne";
+import ClassMouse from "./components/ClassMouse";
+import DataFetching from "./components/DataFetching";
+import HookCounter from "./components/HookCounter";
+import HookCounterFour from "./components/HookCounterFour";
+import HookCounterOne from "./components/HookCounterOne";
+import HookCounterThree from "./components/HookCounterThree";
+import HookCounterTwo from "./components/HookCounterTwo";
+import HookMouse from "./components/HookMouse";
+import IntervalClassCounter from "./components/IntervalClassCounter";
+import IntervalHookCounter from "./components/IntervalHookCounter";
+import MouseContainer from "./components/MouseContainer";
 
 function App() {
   return (
@@ -28,6 +28,7 @@ function App() {
       <HookCounterFour />
       <IntervalClassCounter />
       <IntervalHookCounter />
+      <DataFetching />
     </div>
   );
 }
