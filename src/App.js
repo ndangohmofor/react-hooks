a1baa1b0d7b1579413acf8b9d39e63d4c7dc9ed0
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-09-02 00:04:04 -0400

    Added useEffect and demonstrated dependency arrays

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import ClassCounterOne from './components/ClassCounterOne';
+import ClassMouse from './components/ClassMouse';
 import HookCounter from './components/HookCounter';
 import HookCounterFour from './components/HookCounterFour';
 import HookCounterOne from './components/HookCounterOne';
 import HookCounterThree from './components/HookCounterThree';
 import HookCounterTwo from './components/HookCounterTwo';
+import HookMouse from './components/HookMouse';
+import IntervalClassCounter from './components/IntervalClassCounter';
+import IntervalHookCounter from './components/IntervalHookCounter';
+import MouseContainer from './components/MouseContainer';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounterOne /> */}
+      {/* <ClassMouse /> */}
+      {/* <HookMouse /> */}
+      <MouseContainer />
       <HookCounterOne />
       <ClassCounter />
       <HookCounter />
       <HookCounterTwo />
       <HookCounterThree />
       <HookCounterFour />
+      <IntervalClassCounter />
+      <IntervalHookCounter />
     </div>
   );
 }
--- src/components/HookCounterOne.js
+++ src/components/HookCounterOne.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react"
 
 const HookCounterOne = () => {
     const [count, setCounter] = useState(0);
+    const [name, setName] = useState('');
 
     useEffect(() => {
+        console.log("useEffect - updating document title")
         document.title = `You Clicked ${count} times`
     }, [count])
 
   return (
     <div>
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
       <button onClick={() => setCounter(count + 1)}>
         Click {count} times
       </button>
