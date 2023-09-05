898e45271f11fdf81fd249d09414e6d1a9cc2727
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-09-05 00:22:24 -0400

    Combining useReducer and useContext for global state management

--- src/components/ComponentF.js
+++ src/components/ComponentF.js
@@ -1,24 +1,18 @@
-import React from "react";
-import { UserContext, ChannelContext } from "../App";
+import React, { useContext } from "react";
+import { CountContext } from "../App";
 
 const ComponentF = () => {
+  const countContext = useContext(CountContext);
   return (
     <div>
-      <UserContext.Consumer>
-        {(user) => {
-          return (
-            <ChannelContext.Consumer>
-              {(channel) => {
-                return (
-                  <div>
-                    User context value {user}, channel context value {channel}
-                  </div>
-                );
-              }}
-            </ChannelContext.Consumer>
-          );
-        }}
-      </UserContext.Consumer>
+      Component F - {countContext.countState}
+      <button onClick={() => countContext.countDispatch("increment")}>
+        Increment
+      </button>
+      <button onClick={() => countContext.countDispatch("decrement")}>
+        Decrement
+      </button>
+      <button onClick={() => countContext.countDispatch("reset")}>Reset</button>
     </div>
   );
 };
